def36b7424983c6b5d867ab986842f3bb2d1470e
Vlad <pikova98@bk.ru>
2023-05-09 21:02:50 +0500

    moved samples to separate class

--- MeasurePerformance.cs
+++ MeasurePerformance.cs
@@ -7,6 +7,44 @@ namespace AdamOptimizer
 {
     public static class MeasurePerformance
     {
+        public static Func<IDataAccess<double>, double>[] Functions ={
+                (d)=>Math.Abs(d[0]-d[1]*d[2]+Math.Exp(d[0]*d[1]*d[2])-Math.Sin(d[0]+d[1]+d[2])),
+                (d)=>Math.Abs(d[0]-d[1]*d[2]),
+                (d)=>Math.Abs(d[0]*d[1]*d[2]-d[0]*Math.Sin(d[2])+Math.Cos(d[0]*d[2])+d[2]),
+                (d)=>Math.Abs(d[0]*Math.Cosh(d[0]*d[1]+Math.Sin(d[2]))),
+                (d)=>Math.Abs(Math.Tan(d[0]*d[1]+d[2])-Math.Sin(d[1]*d[2]+d[0])),
+                (d)=>Math.Abs(Math.Pow(d[0]*d[0],d[1])-d[2]*Math.Exp(d[0])*Math.Log(d[2]*d[2]))
+            };
+        public static void PrintBothAdamAndMinePerformance(Func<IDataAccess<double>, double> func)
+        {
+            //choose function to find it's minima
+            // var func = (double a, double b, double c)=>Math.Abs(Functions.Sum(x=>x(a,b,c)));
+
+            ArrayDataAccess<double> variables1 = new double[] { Random.Shared.NextDouble(), Random.Shared.NextDouble(), Random.Shared.NextDouble() };
+            ArrayDataAccess<double> variables2 = new double[3];
+            variables1.Array.CopyTo(variables2, 0);
+
+            var gradientDescent1 = new GradientDescent(variables1, func);
+            var gradientDescent2 = new GradientDescent(variables2, func);
+
+            var before = func(variables1);
+
+            var maxIterations = 40;
+            var learningRate = 0.1;
+            var theta = 0.0001;
+
+            System.Console.WriteLine(gradientDescent1.AdamDescent(maxIterations, learningRate, theta) + " Adam iterations");
+            System.Console.WriteLine(gradientDescent2.MineDescent(maxIterations, learningRate, theta) + " Mine iterations");
+            var after1 = func(variables1);
+            var after2 = func(variables2);
+
+            Console.WriteLine("Before " + before);
+            Console.WriteLine("Adam " + after1);
+            Console.WriteLine("Mine " + after2);
+            System.Console.WriteLine("Adam Point is [" + String.Join(' ', variables1) + "]");
+            System.Console.WriteLine("Mine Point is [" + String.Join(' ', variables2) + "]");
+
+        }
         public static void Measure(Func<IDataAccess<double>, double> func)
         {
             ArrayDataAccess<double> variables1 = new double[] { Random.Shared.NextDouble(), Random.Shared.NextDouble(), Random.Shared.NextDouble() };
@@ -30,7 +68,7 @@ namespace AdamOptimizer
                 variables1.Array.CopyTo(variables2, 0);
 
                 before = func(variables1);
-                gradientDescent1 = new GradientDescent(variables1,func);
+                gradientDescent1 = new GradientDescent(variables1, func);
                 gradientDescent2 = new GradientDescent(variables2, func);
                 gradientDescent1.AdamDescent(maxIterations, learningRate, theta);
                 gradientDescent2.MineDescent(maxIterations, learningRate, theta);
--- Program.cs
+++ Program.cs
@@ -1,42 +1,5 @@
 ﻿using AdamOptimizer;
-Func<double, double, double, double>[] functions ={
-     (a,b,c)=>a-b*c+Math.Exp(a*b*c)-Math.Sin(a+b+c),
-     (a,b,c)=>a-b*c,
-     (a,b,c)=>a*b*c-a*Math.Sin(c)+Math.Cos(a*b)+c,
-     (a,b,c)=>a*Math.Cosh(a*b+Math.Sin(c)),
-     (a,b,c)=>Math.Tan(a*b+c)-Math.Sin(b*c+a),
-     (a,b,c)=>Math.Pow(a*a,b)-c*Math.Exp(a)*Math.Log(c*c)
-};
 
-//choose function to find it's minima
-// var func = (double a, double b, double c)=>Math.Abs(functions.Sum(x=>x(a,b,c)));
-var func = (double a, double b, double c) => Math.Abs(functions[5](a, b, c));
-
-var functionToFeed = (IDataAccess<double> x) => func(x[0], x[1], x[2]);
-
-ArrayDataAccess<double> variables1 = new double[] { Random.Shared.NextDouble(), Random.Shared.NextDouble(), Random.Shared.NextDouble() };
-ArrayDataAccess<double> variables2 = new double[3];
-variables1.Array.CopyTo(variables2, 0);
-
-var gradientDescent1 = new GradientDescent(variables1, functionToFeed);
-var gradientDescent2 = new GradientDescent(variables2, functionToFeed);
-
-var before = func(variables1[0], variables1[1], variables1[2]);
-
-var maxIterations = 40;
-var learningRate = 0.1;
-var theta = 0.0001;
-
-System.Console.WriteLine(gradientDescent1.AdamDescent(maxIterations, learningRate, theta) + " Adam iterations");
-System.Console.WriteLine(gradientDescent2.MineDescent(maxIterations, learningRate, theta) + " Mine iterations");
-var after1 = functionToFeed(variables1);
-var after2 = functionToFeed(variables2);
-
-Console.WriteLine("Before " + before);
-Console.WriteLine("Adam " + after1);
-Console.WriteLine("Mine " + after2);
-System.Console.WriteLine("Adam Point is [" + String.Join(' ', variables1) + "]");
-System.Console.WriteLine("Mine Point is [" + String.Join(' ', variables2) + "]");
-
-MeasurePerformance.Measure(functionToFeed);
+MeasurePerformance.PrintBothAdamAndMinePerformance(MeasurePerformance.Functions[2]);
+MeasurePerformance.Measure(MeasurePerformance.Functions[2]);
 
