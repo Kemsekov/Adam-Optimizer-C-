da2c57b7bb561a63833be34931f4cca74c5637d8
Vlad <pikova98@bk.ru>
2023-05-10 12:13:19 +0500

    apparently fixed adam optimizer to work as it is intended, but now it performs even more worse than before =(

--- GradientDescent.cs
+++ GradientDescent.cs
@@ -13,11 +13,16 @@ public class GradientDescent
     public double Beta1 = 0.9;
     public double Beta2 = 0.99;
     public double Epsilon = 1e-6;
+    public double LearningRate;
+    public double Theta;
+
     public GradientDescent(IDataAccess<double> variables, Func<IDataAccess<double>, double> function)
     {
         Function = function;
         Variables = variables;
         Dimensions = variables.Length;
+        firstMomentum = new double[Dimensions];
+        secondMomentum = new double[Dimensions];
     }
     double Evaluate(IDataAccess<double> variables)
     {
@@ -42,38 +47,30 @@ public class GradientDescent
             change[i] *= coefficient;
         }
     }
-    void ComputeChangeAdam(IDataAccess<double> change, double learningRate, double currentEvaluation)
+    double[] firstMomentum;
+    double[] secondMomentum;
+    void ComputeChangeAdam(IDataAccess<double> change, double learningRate, double currentEvaluation, int iteration)
     {
         ComputeGradient(change, currentEvaluation);
         var gradient = change;
-        int t = 0; // The timestep counter
-
-        // Initialize the first and second moment vector
+        int t = iteration; // The timestep counter
 
         // Loop over the parameters
-        double previousFirstMomentum = 0;
-        double previousSecondMomentum = 0;
-        double firstMomentum;
-        double secondMomentum;
+
         for (int i = 0; i < Dimensions; i++)
         {
-            // Increment the timestep counter
-            t++;
-
             // Update the biased first moment estimate
-            firstMomentum = Beta1 * previousFirstMomentum + (1 - Beta1) * gradient[i];
+            firstMomentum[i] = Beta1 * firstMomentum[i] + (1 - Beta1) * gradient[i];
 
             // Update the biased second moment estimate
-            secondMomentum = Beta2 * previousSecondMomentum + (1 - Beta2) * gradient[i] * gradient[i];
+            secondMomentum[i] = Beta2 * secondMomentum[i] + (1 - Beta2) * gradient[i] * gradient[i];
 
             // Compute the bias-corrected first moment estimate
-            double m_hat = firstMomentum / (1 - Math.Pow(Beta1, t));
+            double m_hat = firstMomentum[i] / (1 - Math.Pow(Beta1, t));
 
             // Compute the bias-corrected second moment estimate
-            double v_hat = secondMomentum / (1 - Math.Pow(Beta2, t));
+            double v_hat = secondMomentum[i] / (1 - Math.Pow(Beta2, t));
 
-            previousFirstMomentum = firstMomentum;
-            previousSecondMomentum = secondMomentum;
             // Update the parameters    
             change[i] = learningRate * m_hat / (Math.Sqrt(v_hat) + Epsilon);
         }
@@ -81,34 +78,26 @@ public class GradientDescent
     void Step(IDataAccess<double> change)
     {
         for (var i = 0; i < Dimensions; i++)
-        {
-            var c = change[i];
-            if (c == 0) continue;
-            Variables[i] -= c;
-        }
+            Variables[i] -= change[i];
     }
     void UndoStep(IDataAccess<double> change)
     {
         for (var i = 0; i < Dimensions; i++)
-        {
-            var c = change[i];
-            if (c == 0) continue;
-            Variables[i] += c;
-        }
+            Variables[i] += change[i];
     }
-    public int MineDescent(int maxIterations, double learningRate = 1, double theta = 0.001)
+    public int MineDescent(int maxIterations)
     {
         using RentedArrayDataAccess<double> change = new(ArrayPoolStorage.RentArray<double>(Dimensions));
         var iterations = 0;
-        while (maxIterations-- > 0)
+        var learningRate = LearningRate;
+        while (iterations++<maxIterations)
         {
-            iterations++;
             var beforeStep = Evaluate(Variables);
             ComputeChangeMine(change, learningRate, beforeStep);
             Step(change);
             var afterStep = Evaluate(Variables);
             var diff = Math.Abs(afterStep - beforeStep);
-            if (diff <= theta) break;
+            if (diff <= Theta) break;
             if (afterStep >= beforeStep)
             {
                 UndoStep(change);
@@ -117,19 +106,19 @@ public class GradientDescent
         }
         return iterations;
     }
-    public int AdamDescent(int maxIterations, double learningRate = 1, double theta = 0.001)
+    public int AdamDescent(int maxIterations)
     {
         using RentedArrayDataAccess<double> change = new(ArrayPoolStorage.RentArray<double>(Dimensions));
         var iterations = 0;
-        while (maxIterations-- > 0)
+        var learningRate = LearningRate;
+        while (iterations++<maxIterations)
         {
-            iterations++;
             var beforeStep = Evaluate(Variables);
-            ComputeChangeAdam(change, learningRate, beforeStep);
+            ComputeChangeAdam(change, learningRate, beforeStep,iterations);
             Step(change);
             var afterStep = Evaluate(Variables);
             var diff = Math.Abs(afterStep - beforeStep);
-            if (diff <= theta) break;
+            // if (diff <= Theta) break;    
             if (afterStep >= beforeStep)
             {
                 UndoStep(change);
